95557d01158e0a5d78f9e18b6da93b4f0721d7ab
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-22 12:46:27 +0300

    feat: add key to grid element

--- src/common/GridTemplate/GridTemplate.jsx
+++ src/common/GridTemplate/GridTemplate.jsx
@@ -4,8 +4,8 @@ import { GridElementTemplate } from './components/GridElementTemplate';
 const GridTemplate = ({ children }) => {
   return (
     <div className='row row-cols-2 mt-5 gy-4 gx-5'>
-      {children.map((child) => (
-        <GridElementTemplate>{child}</GridElementTemplate>
+      {children.map((child, i) => (
+        <GridElementTemplate key={i}>{child}</GridElementTemplate>
       ))}
     </div>
   );
