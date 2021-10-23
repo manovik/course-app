cb49b41dc8dab8ef9c754a9f50deeb7103fe49d9
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-23 17:03:32 +0300

    feat: remove mapCoursesWithUsers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,13 +1,6 @@
 import { convertMinutesToTime } from './convertTime';
 import { convertDate } from './convertDate';
-import { mapCoursesWithUsers } from './mapCoursesWithUsers';
 import { validateCourseFields } from './validateCourseFields';
 import { callAlert } from './callAlert';
 
-export {
-  convertMinutesToTime,
-  convertDate,
-  mapCoursesWithUsers,
-  validateCourseFields,
-  callAlert,
-};
+export { convertMinutesToTime, convertDate, validateCourseFields, callAlert };
