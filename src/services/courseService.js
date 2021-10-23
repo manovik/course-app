3af96c24df5dbe85a63e6cbf3eaf5eac39035f1d
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-23 16:57:08 +0300

    feat: create imitation of course service

---
+++ src/services/courseService.js
@@ -0,0 +1,47 @@
+import { v4 } from 'uuid';
+import { AuthorService } from '@services';
+import { mockedAuthorsList } from '@mock/mockedAuthorsList';
+
+// imitation of some user service
+
+class CourseService {
+  constructor(courseService) {
+    this.courseService = courseService;
+  }
+
+  createNewCourse = (courseInfo) => ({
+    ...courseInfo,
+    id: this.generatUUID(),
+    creationDate: new Date().toLocaleDateString('en-US'),
+  });
+
+  generatUUID = () => v4();
+
+  add = (newCourse) => {
+    this.courseService.push(newCourse);
+  };
+
+  getAll = () => [...this.courseService];
+
+  getMappedCoursesOnAuthors = () => {
+    const authorService = new AuthorService(mockedAuthorsList);
+    const authorsList = authorService.getAll();
+    return this.courseService.map((course) => {
+      const authors = [];
+      for (const id of course.authors) {
+        const author =
+          authorsList.find((a) => {
+            return a.id === id;
+          }).name || 'Unknown';
+        authors.push(author);
+      }
+
+      return {
+        ...course,
+        authors,
+      };
+    });
+  };
+}
+
+export default CourseService;
--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,2 @@
 export { default as AuthorService } from './authorService';
+export { default as CourseService } from './courseService';
