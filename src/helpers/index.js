69d9cf4b11693f345c74d87a9aa9e291bb01afb7
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-26 14:15:33 +0300

    refactor: edit import/export paths

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,4 @@
-import { convertMinutesToTime } from './convertTime';
-import { convertDate } from './convertDate';
-import { validateCourseFields } from './validateCourseFields';
-import { callAlert } from './callAlert';
-
-export { convertMinutesToTime, convertDate, validateCourseFields, callAlert };
+export { convertMinutesToTime } from './convertTime';
+export { convertDate } from './convertDate';
+export { validateCourseFields } from './validateCourseFields';
+export { callAlert } from './callAlert';
