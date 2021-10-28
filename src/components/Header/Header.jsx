eba4237ffee0a26058ad3c39ea5590185ef29802
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-29 01:39:14 +0300

    feat: add logout for header, edit routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { Button } from 'common/Button';
 
 import { Logo } from './components/Logo';
 import { User } from './components/User';
 
+import { APP } from 'utils/appRoutes';
+
 import './header.scss';
 
-const Header = ({ isLoggedIn }) => {
+const Header = ({ isLoggedIn, setIsLoggedIn }) => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem('u-token');
+    setIsLoggedIn(false);
+    history.push(APP.LOGIN);
+  };
+
   return (
     <header className='header shadow p-3 mb-5'>
       <div className='container'>
         <div className='d-flex align-items-center'>
           <div className='col'>
-            <Link to='/'>
+            <Link to={APP.ROOT}>
               <Logo />
             </Link>
           </div>
@@ -28,6 +38,7 @@ const Header = ({ isLoggedIn }) => {
                 <Button
                   btnClassName='btn-outline-secondary btn-md fs-5 c-dark'
                   buttonText={'Logout'}
+                  onClick={handleLogout}
                 />
               </div>
             </>
