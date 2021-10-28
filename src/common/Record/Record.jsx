f0949447ebeb5c8bb77f2bfa4d0102931d3e3b9a
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-28 18:29:10 +0300

    feat: move children lower in element

--- src/common/Record/Record.jsx
+++ src/common/Record/Record.jsx
@@ -1,23 +1,27 @@
 import './record.scss';
 
 const Record = ({ caption, text, title, children }) => (
-  <p className='d-flex'>
-    <span className='fw-bold'>{caption}:</span>&nbsp;
-    <span
-      className={title ? 'tip' : ''}
-      style={{
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-      }}
-      title={title}
-    >
-      {text}
-      {children?.map((ch, i) => (
-        <p key={ch.replace(/\s/g, i)}>{ch}</p>
-      ))}
-    </span>
-  </p>
+  <>
+    <p className='d-flex'>
+      <span className='fw-bold'>{caption}:</span>&nbsp;
+      <span
+        className={title ? 'tip' : ''}
+        style={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+        title={title}
+      >
+        {text}
+      </span>
+    </p>
+    {children?.map((ch, i) => (
+      <span style={{ display: 'block' }} key={ch.replace(/\s/g, i)}>
+        {ch}
+      </span>
+    ))}
+  </>
 );
 
 export default Record;
