fba5b6213443e9a017abb98c06d8bc3bec4edb93
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-11-23 01:21:07 +0300

    fix: edit courses test

--- src/components/Courses/__tests__/courses.test.jsx
+++ src/components/Courses/__tests__/courses.test.jsx
@@ -11,77 +11,75 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('Testing Courses', () => {
-  describe('Testing Courses', () => {
-    it('should render no courses', () => {
-      const myState = {
-        authors: mockedAuthorsList,
-        courses: [],
-        user: {
-          name: 'Alex',
-          isAuth: true,
-          role: 'admin',
-        },
-        app: {
-          isLoading: false,
-          errors: [],
-          firstAppLoad: true,
-        },
-      };
+  it('should render no courses', () => {
+    const myState = {
+      authors: mockedAuthorsList,
+      courses: [],
+      user: {
+        name: 'Alex',
+        isAuth: true,
+        role: 'admin',
+      },
+      app: {
+        isLoading: false,
+        errors: [],
+        firstAppLoad: true,
+      },
+    };
 
-      const store = {
-        getState: () => myState,
-        subscribe: jest.fn(),
-        dispatch: jest.fn(),
-      };
+    const store = {
+      getState: () => myState,
+      subscribe: jest.fn(),
+      dispatch: jest.fn(),
+    };
 
-      const { getByTestId } = render(
-        <Wrapper store={store}>
-          <Courses></Courses>
-        </Wrapper>
-      );
+    const { getByTestId } = render(
+      <Wrapper store={store}>
+        <Courses />
+      </Wrapper>
+    );
 
-      expect(getByTestId(testIds.COURSE_WRAPPER)).toBeEmptyDOMElement();
+    expect(getByTestId(testIds.COURSE_WRAPPER)).toBeEmptyDOMElement();
+  });
+
+  it('should render exact quantity of courses from store', () => {
+    const store = {
+      getState: () => state,
+      subscribe: jest.fn(),
+      dispatch: jest.fn(),
+    };
+
+    const { getAllByRole } = render(
+      <Wrapper store={store}>
+        <Courses />
+      </Wrapper>
+    );
+
+    expect(getAllByRole('article')).toHaveLength(2);
+  });
+
+  it('should be showed after a click on a button "Add new course"', () => {
+    const pushMock = jest.fn();
+    rrd.useHistory.mockReturnValue({
+      push: pushMock,
     });
 
-    it('should render exact quantity of courses from store', () => {
-      const store = {
-        getState: () => state,
-        subscribe: jest.fn(),
-        dispatch: jest.fn(),
-      };
+    const store = {
+      getState: () => state,
+      subscribe: jest.fn(),
+      dispatch: jest.fn(),
+    };
 
-      const { getAllByRole } = render(
-        <Wrapper store={store}>
-          <Courses></Courses>
-        </Wrapper>
-      );
+    const { getByTestId } = render(
+      <Wrapper store={store}>
+        <Courses />
+      </Wrapper>
+    );
 
-      expect(getAllByRole('article')).toHaveLength(2);
-    });
-
-    it('should be showed after a click on a button "Add new course"', () => {
-      const pushMock = jest.fn();
-      rrd.useHistory.mockReturnValue({
-        push: pushMock,
-      });
-
-      const store = {
-        getState: () => state,
-        subscribe: jest.fn(),
-        dispatch: jest.fn(),
-      };
-
-      const { getByTestId } = render(
-        <Wrapper store={store}>
-          <Courses />
-        </Wrapper>
-      );
-
-      const addCourseBtn = getByTestId(testIds.ADD_BTN);
-      fireEvent.click(addCourseBtn);
-      const expectedRoute = `${APP.COURSES_ADD}`;
-      expect(addCourseBtn).toBeInTheDocument();
-      expect(pushMock).toHaveBeenCalledWith(expectedRoute);
-    });
+    const addCourseBtn = getByTestId(testIds.ADD_BTN);
+    fireEvent.click(addCourseBtn);
+    const expectedRoute = `${APP.COURSES_ADD}`;
+    expect(addCourseBtn).toBeInTheDocument();
+    expect(pushMock).toHaveBeenCalledWith(expectedRoute);
   });
 });
